734ee23eac305acf35078a2645f989c66ecb0270
Artem <temazasport@mail.ru>
2022-11-25 15:01:08 +0800

    HW_task_2

---
+++ HW_Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+float SetNumber(string str)
+{
+
+    System.Console.WriteLine($"Введите {str}");
+    float num = int.Parse(Console.ReadLine());
+
+    return num;
+}
+
+System.Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+
+float numK1 = SetNumber("k1");
+float numB1= SetNumber("b1");
+float numK2 = SetNumber("k2");
+float numB2 = SetNumber("b2");
+
+float x = (numB2-numB1)/(numK1-numK2);
+
+System.Console.WriteLine($"точка переcечения -> ({x},{numK1*x + numB1})");
