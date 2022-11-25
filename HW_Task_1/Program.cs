f7450708dbe081c9ffa74ef2a754f8a151102bb7
Artem <temazasport@mail.ru>
2022-11-25 15:58:23 +0800

    HW_task_1

---
+++ HW_Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+System.Console.WriteLine("Введите числа");
+
+string str = Console.ReadLine();
+string[] strArr = str.Split(' ');
+
+int count = 0;
+
+int[] arr = new int[strArr.Length];
+
+for (int i = 0; i < strArr.Length; i++)
+{
+    arr[i] = int.Parse(strArr[i]);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]>0)
+    {
+        count++;
+    }
+}
+
+System.Console.WriteLine($"количество чисел больше 0 = {count}");
